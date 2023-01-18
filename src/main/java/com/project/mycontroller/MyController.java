bb6e4a5804c9c26a27a3911aa0254d2ffea5c2ac
admane2000 <maheshadmanefb@gmail.com>
2023-01-18 22:46:35 +0530

    creating controller package and first controller java class

---
+++ src/main/java/com/project/mycontroller/MyController.java
@@ -0,0 +1,10 @@
+package com.project.mycontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	//this is rest controller
+	
+
+}
